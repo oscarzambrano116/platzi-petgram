6bec3b8b2a1f240f87d0d5c2f60d89fb40e66a0d
Oscar Zambrano <oscarz.developer@gmail.com>
2019-10-20 19:59:49 -0300

    Adding Category component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { App } from './App'
 
 ReactDom.render(
-  <h1>React App Start...</h1>,
+  <App />,
   document.querySelector('#app')
 )
