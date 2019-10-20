d7ae0079c3b55913c0f1717aeb8012827b3d9a69
Oscar Zambrano <oscarz.developer@gmail.com>
2019-10-20 20:27:08 -0300

    Importing categories data in ListOfCategories component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Category } from './components/Category'
+import { GlobalStyle } from './GlobalStyle'
+import { ListOfCategories } from './components/ListOfCategories'
 
 export const App = () => {
   return (
-    <Category />
+    <>
+      <GlobalStyle />
+      <ListOfCategories />
+    </>
   )
 }
