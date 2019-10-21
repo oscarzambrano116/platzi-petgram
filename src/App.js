456aa524e30c3e15845dbb9767ed5392543c51f3
Oscar Zambrano <oscarz.developer@gmail.com>
2019-10-21 19:10:32 -0300

    Adding ListOfPhotoCards component and Logo component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { GlobalStyle } from './GlobalStyle'
+import { Logo } from './components/Logo'
 import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 export const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Logo />
       <ListOfCategories />
+      <ListOfPhotoCards />
     </>
   )
 }
---
+++ src/components/ListOfPhotoCards/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { PhotoCard } from '../PhotoCard'
+
+export const ListOfPhotoCards = (
+) => {
+  return (
+    <ul>
+      {
+        [1, 2, 3].map((id) => (
+          <li key={id}>
+            <PhotoCard />
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
