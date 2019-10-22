57563eb143701f18b674d35863c0307efade58ef
Oscar Zambrano <oscarz.developer@gmail.com>
2019-10-22 20:19:45 -0300

    Adding categories fix component

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setshowFixed] = useState(false)
+
+  useEffect(() => {
+    window.fetch('https://petgram-api-oscarzambrano116.now.sh/categories')
+      .then((res) => res.json())
+      .then((response) => setCategories(response))
+  }, [])
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setshowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map((category) => (
           <Item key={category.id}>
@@ -15,5 +34,12 @@ export const ListOfCategories = () => {
         ))
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
