7ac8c9a66ea2fe0993ee7bbcf860698a5b7a36ef
Oscar Zambrano <oscarz.developer@gmail.com>
2019-11-10 18:27:46 -0300

    Adding NotRegisteredUser page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Detail } from './pages/Detail'
 import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 
 export const App = () => {
   return (
@@ -16,6 +23,23 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) => {
+            return isAuth ? (
+              <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+            ) : (
+              <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+            )
+          }
+        }
+      </UserLogged>
       <NavBar />
     </>
   )
