0da76596b6188d103b41d6a887491206e73ab9a2
Oscar Zambrano <oscarz.developer@gmail.com>
2019-11-17 18:19:32 -0300

    Adding Login Mutation

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../containers/RegisterMutation'
+import { LoginMutation } from '../containers/LoginMutation'
 
 export const NotRegisteredUser = () => {
   return (
@@ -36,7 +37,32 @@ export const NotRegisteredUser = () => {
                   }
                 }
               </RegisterMutation>
-              <UserForm onSubmit={activateAuth} title='Log In' />
+              <LoginMutation>
+                {
+                  (login, { data, loading, error }) => {
+                    const onSubmit = ({ email, password }) => {
+                      const variables = {
+                        input: {
+                          email,
+                          password
+                        }
+                      }
+                      login({ variables }).then(activateAuth)
+                    }
+
+                    const errorMsg = error && 'Error in login!'
+
+                    return (
+                      <UserForm
+                        onSubmit={onSubmit}
+                        title='Log In'
+                        error={errorMsg}
+                        disabled={loading}
+                      />
+                    )
+                  }
+                }
+              </LoginMutation>
             </>
           )
         }
